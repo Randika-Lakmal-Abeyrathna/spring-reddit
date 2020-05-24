be174d7527ea47f746de6e89b42e0da5f5a51858
Randika <randika.help@gmail.com>
2020-05-24 23:40:12 +0530

    User Registration Complete ** Email Sent ** Not Completed the Activation Link Part

---
+++ src/main/java/com/randikalakmal/springreddit/service/MailService.java
@@ -0,0 +1,40 @@
+package com.randikalakmal.springreddit.service;
+
+import com.randikalakmal.springreddit.exception.SpringRedditException;
+import com.randikalakmal.springreddit.model.NotificationEmail;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+public class MailService {
+
+    private final JavaMailSender javaMailSender;
+    private final MailContentBuilder mailContentBuilder;
+
+    public void sendMail(NotificationEmail notificationEmail){
+
+        MimeMessagePreparator messagePreparator = mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setFrom("randika.test@email.com");
+            messageHelper.setTo(notificationEmail.getRecipient());
+            messageHelper.setSubject(notificationEmail.getSubject());
+            messageHelper.setText(mailContentBuilder.build(notificationEmail.getBody()));
+        };
+
+        try{
+            javaMailSender.send(messagePreparator);
+            log.info("Activation email sent !");
+        }catch(MailException e){
+            //e.printStackTrace();
+            throw new SpringRedditException("Exception occurred when sending email to " + notificationEmail.getRecipient());
+        }
+
+    }
+}
